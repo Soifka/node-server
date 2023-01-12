a075bbcc009e185525d897a1b9753356ea7ff58f
Julia Alekseeva <soifka@gmail.com>
2023-01-12 19:48:58 +0200

    Server response

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const PORT = 3000;
 
 const requestListener = (req, res) => {
     //console.log('REQUEST IS HERE')
-    const { url, method } = req;
-    console.log(url, method);
+    // const { url, method } = req;
+    //console.log(url, method);
+    res.statusCode = 418;
+    res.end('HELLO FROM SERVER');
 }
 
 const server = http.createServer(requestListener);
