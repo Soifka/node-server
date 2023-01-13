7099465d7f326a00e81b4363fb63a3d60dacd637
Julia Alekseeva <soifka@gmail.com>
2023-01-13 18:41:29 +0200

    Express welcome

--- index.js
+++ index.js
@@ -1,69 +1,11 @@
-const http = require("http");
-const fs = require("fs/promises");
-
+const express = require('express');
+const app = express();
 const PORT = 3000;
 
-const requestListener = async (req, res) => {
-  const { url, method } = req;
+app.get('/', (req, res) => {
+  res.send('Hello, world');
+});
 
-  if(method === 'GET') {
-    if (url === "/index.html") {
-      try {
-        const data = await fs.readFile("./views/index.html", "utf8");
-        res.end(data);
-      } catch(err) {
-          res.statusCode = 404;
-          res.end();
-      }
-    } else if (url === "/style.css") {
-        try {
-            const data = await fs.readFile("./views/style.css", "utf8");
-            res.end(data);
-          } catch(err) {
-              res.statusCode = 404;
-              res.end();
-          }
-    } else {
-        try {
-            const error = await fs.readFile("./views/error.html", "utf8");
-            res.end(error);
-        } catch(err) {
-            res.statusCode = 404;
-            res.end();
-        }
-    }
-  } else if(method === 'POST') {
-      if(url === '/user') {
-        let jsonString = '';
-        req.on('data', (chunk) => {
-          jsonString += chunk;
-        });
-        req.on('end', () => {
-          const user = JSON.parse(jsonString);
-          console.log(user);
-        })
-      }
-  } else {
-      res.statusCode = 404;
-      res.end();    
-  }
-
-
-};
-
-/*
-  if (url === "/index.html") {
-    fs.readFile("./views/index.html", "utf8").then((data) => {
-    res.end(data);
-    });
-  } else {
-    res.statusCode = 404;
-    fs.readFile("./views/error.html", "utf8").then((data) => {
-      res.end(data);
-    });
-  }
-  */
-
-const server = http.createServer(requestListener);
-
-server.listen(PORT); // указали серверу, что он должен прослушивать 3000 порт
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
