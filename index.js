1f5347f3c0cccd17e42bb0351d1ba4e4306d51be
Julia Alekseeva <soifka@gmail.com>
2023-01-13 18:23:06 +0200

    POST method reading user

--- index.js
+++ index.js
@@ -4,33 +4,51 @@ const fs = require("fs/promises");
 const PORT = 3000;
 
 const requestListener = async (req, res) => {
-  const { url } = req;
+  const { url, method } = req;
 
-  if (url === "/index.html") {
-    try {
-      const data = await fs.readFile("./views/index.html", "utf8");
-      res.end(data);
-    } catch(err) {
-        res.statusCode = 404;
-        res.end();
-    }
-  } else if (url === "/style.css") {
-    try {
-        const data = await fs.readFile("./views/style.css", "utf8");
+  if(method === 'GET') {
+    if (url === "/index.html") {
+      try {
+        const data = await fs.readFile("./views/index.html", "utf8");
         res.end(data);
       } catch(err) {
           res.statusCode = 404;
           res.end();
       }
-  } else {
-    try {
-        const error = await fs.readFile("./views/error.html", "utf8");
-        res.end(error);
-    } catch(err) {
-        res.statusCode = 404;
-        res.end();
+    } else if (url === "/style.css") {
+        try {
+            const data = await fs.readFile("./views/style.css", "utf8");
+            res.end(data);
+          } catch(err) {
+              res.statusCode = 404;
+              res.end();
+          }
+    } else {
+        try {
+            const error = await fs.readFile("./views/error.html", "utf8");
+            res.end(error);
+        } catch(err) {
+            res.statusCode = 404;
+            res.end();
+        }
     }
+  } else if(method === 'POST') {
+      if(url === '/user') {
+        let jsonString = '';
+        req.on('data', (chunk) => {
+          jsonString += chunk;
+        });
+        req.on('end', () => {
+          const user = JSON.parse(jsonString);
+          console.log(user);
+        })
+      }
+  } else {
+      res.statusCode = 404;
+      res.end();    
   }
+
+
 };
 
 /*
